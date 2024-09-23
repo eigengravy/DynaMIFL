58780e87db1f0908e9e0fd201b70856c9066c1d4
Sarang S <email.sarangs@gmail.com>
2024-09-23 18:21:12 +0530

    feat: basic workload

---
+++ datasets/cifar100.py
@@ -0,0 +1,46 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
+from flwr_datasets import FederatedDataset
+
+
+def load_dataset(partitioners, batch_size=64, test_size=0.1):
+    fds = FederatedDataset(
+        dataset="cifar100",
+        partitioners=partitioners,
+    )
+
+    def apply_transforms(batch):
+
+        batch["img"] = [
+            Compose(
+                [
+                    ToTensor(),
+                    Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
+                    Lambda(lambda x: x.repeat(3, 1, 1) if x.size(0) == 1 else x),
+                ]
+            )(img)
+            for img in batch["img"]
+        ]
+        return batch
+
+    testloader = DataLoader(
+        fds.load_split("test").with_transform(apply_transforms), batch_size=batch_size
+    )
+
+    def get_client_loader(cid: str):
+        client_dataset = fds.load_partition(int(cid), "train")
+        client_dataset_splits = client_dataset.train_test_split(
+            test_size=test_size, seed=42
+        )
+        trainset = client_dataset_splits["train"]
+        valset = client_dataset_splits["test"]
+        trainloader = DataLoader(
+            trainset.with_transform(apply_transforms), batch_size=batch_size
+        )
+        valloader = DataLoader(
+            valset.with_transform(apply_transforms), batch_size=batch_size
+        )
+        return trainloader, valloader
+
+    return testloader, get_client_loader
